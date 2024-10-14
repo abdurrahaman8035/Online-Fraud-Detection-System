0e5863053f7a8e4fc0cb3ce6ba1a12fa52a536f9
Abdurrahaman Abdullahi <53434773+abdurrahaman8035@users.noreply.github.com>
2024-10-14 05:47:57 +0100

    Update fraud_detection_app.py

--- fraud_detection_app.py
+++ fraud_detection_app.py
@@ -24,6 +24,57 @@ st.write(
 )
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###################### Funtions to transform categorical variable #############################################
 def type_transaction(content):
     if content == "PAYMENT":
@@ -49,11 +100,11 @@ type = st.sidebar.selectbox(" Type of transaction", type_lebels)
 
 step = st.sidebar.slider("Number of Hours it took the Transaction to complete:", min_value = 0, max_value = 744)
 
-amount = st.sidebar.number_input("Amount in Naira",min_value=0, max_value=110000)
-oldbalanceorg = st.sidebar.number_input("""Sender Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalanceorg = st.sidebar.number_input("""Sender Balance After Transaction was made""",min_value=0, max_value=110000)
-oldbalancedest = st.sidebar.number_input("""Recipient Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalancedest = st.sidebar.number_input("""Recipient Balance After Transaction was made""",min_value=0, max_value=110000)
+amount = st.sidebar.number_input("Amount in Naira",min_value=0, max_value=200000000)
+oldbalanceorg = st.sidebar.number_input("""Sender Balance Before Transaction was made""",min_value=0, max_value=1000000)
+newbalanceorg = st.sidebar.number_input("""Sender Balance After Transaction was made""",min_value=0, max_value=1000000)
+oldbalancedest = st.sidebar.number_input("""Recipient Balance Before Transaction was made""",min_value=0, max_value=1000000)
+newbalancedest = st.sidebar.number_input("""Recipient Balance After Transaction was made""",min_value=0, max_value=1000000)
 ## flag 
 
 
